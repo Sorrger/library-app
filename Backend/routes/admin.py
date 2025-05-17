b27e13cae7e97a0eb9371dc171458d9539d5949e
Jupik23 <jupekkekw@gmail.com>
2025-05-17 14:06:02 +0200

    fixed post librarian

--- Backend/routes/admin.py
+++ Backend/routes/admin.py
@@ -35,7 +35,6 @@ def add_admin(admin_data: AccountCreate, db: Session = Depends(get_db)):
         login=admin_data.login.lower(),
         password=hashed_password,
         role=UserRole.admin,
-        student_id=None,
     )
     db.add(new_account)
     db.commit()
@@ -45,13 +44,16 @@ def add_admin(admin_data: AccountCreate, db: Session = Depends(get_db)):
 
 @router.post("/add_librarian")
 def add_librarian(librarian_data: AccountCreate, db: Session = Depends(get_db)):
-    existing_account = db.query(Account).filter(Account.login == librarian_data["login"].lower()).first()
+    existing_account = db.query(Account).filter(Account.login == librarian_data.login.lower()).first()
     if existing_account:
         raise HTTPException(status_code=400, detail="Login already exists.")
 
+    password = librarian_data.password
+    hashed_password = hash_password(password)
+
     new_account = Account(
-        login=librarian_data.login(),
-        password=librarian_data.password,
+        login=librarian_data.login,
+        password=hashed_password,
         role=UserRole.librarian,
     )
     db.add(new_account)
