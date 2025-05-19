60d98a0eeeaaf61fba7ec2b37efb3514a8d9ebf8
Jupik23 <ms306728@student.polsl.pl>
2025-05-19 13:04:27 +0200

    [BE]+Sorrger added book limit value to student, crud + route functionality

--- Backend/models/student.py
+++ Backend/models/student.py
@@ -9,6 +9,7 @@ class Student(Base):
     name = Column(String, nullable=False)
     surname = Column(String, nullable=False)
     phone_number = Column(String, nullable=True)
+    books_limit = Column(Integer, default=0)
 
     account = relationship("Account", back_populates="student", uselist=False) 
     loans = relationship("Loan", back_populates="student") 
--- Backend/routes/student.py
+++ Backend/routes/student.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException
-from schemas.student import StudentCreate, Student
+from schemas.student import StudentCreate, Student, StudentLimitsResponse
 from database.database import get_db
-from crud.student import get_all_students, create_student, get_student_by_id
+from crud.student import get_all_students, create_student, get_student_by_id, increase_books_limit_by_id, decrease_books_limit_by_id
 
 router = APIRouter(tags=['students'])
 
@@ -14,10 +14,23 @@ def get_all_students_endpoint(db = Depends(get_db)):
 def create_student_endpoint(student: StudentCreate, db = Depends(get_db)):
     return create_student(db, student)
 
-@router.get("/students/{student_id}", response_model=Student)
+@router.get("/students/{student_id}", response_model=StudentLimitsResponse)
 def get_student_by_id_endpoint(student_id: int, db = Depends(get_db)):
     db_student = get_student_by_id(db=db, student_id=student_id)
     if db_student is None:
         raise HTTPException(status_code=404, detail="Student not found")
     return db_student
 
+@router.patch("/students/{student_id}/limit-increase", response_model=StudentLimitsResponse)
+def increase_student_limit(student_id: int, db=Depends(get_db)):
+    student = increase_books_limit_by_id(db, student_id)
+    if not student:
+        raise HTTPException(status_code=404, detail="Student not found")
+    return student
+
+@router.patch("/students/{student_id}/limit-decrease", response_model=StudentLimitsResponse)
+def decrease_student_limit(student_id: int, db=Depends(get_db)):
+    student = decrease_books_limit_by_id(db, student_id)
+    if not student:
+        raise HTTPException(status_code=404, detail="Student not found")
+    return student
